118b94cb15b36b855c506b2c38a462d53b58b1fb
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2023-10-18 02:25:50 +0900

    sample

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,12 +1,7 @@
 
-window.addEventListener('load', function() {
-    // 対象のタブのコンソールログに出力される
-    console.log("コンテントスクリプトだよ");
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-        // chrome extension(popup.tsに送り返す例)
-        if (request.greeting == "hello") {
-            sendResponse({farewell: "goodbye"});
-        }
-    });
-})
+alert(getTitle());
+
+export function getTitle() {
+    return document.title;
+};
+
